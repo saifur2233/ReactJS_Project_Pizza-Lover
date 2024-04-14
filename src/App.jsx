8b85af5e412b3dbdaa6b8e09cca48abc3403975c
saifur2233 <saifurnstuiit223344@gmail.com>
2024-04-14 19:36:56 +0600

    Initial Configuration Setup

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import Menu from "./components/Menu.jsx";
+import About from "./components/About.jsx";
+import OtherMenu from "./components/OtherMenu.jsx";
+import Review from "./components/Review.jsx";
+import Footer from "./components/Footer.jsx";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+
+      <div id="home">
+        <Hero />
+      </div>
+
+      <div id="speciality">
+        <Menu />
+      </div>
+
+      <div id="about">
+        <About />
+      </div>
+
+      <div id="menu">
+        <OtherMenu />
+      </div>
+
+      <div id="review">
+        <Review />
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+
+export default App;
